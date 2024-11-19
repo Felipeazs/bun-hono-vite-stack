39db2ac1d08038e49e7bef0a6b5be049627ae3a2
felipeazs <felipeazs@gmail.com>
2024-11-19 10:52:48 -0300

    drizzle config and migrate file

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig, Config } from "drizzle-kit"
+import env from "@/env"
+
+export default defineConfig({
+	dialect: "postgresql",
+	schema: "./server/db/schema.ts",
+	out: "./server/db/migrations",
+	dbCredentials: {
+		url: env.DATABASE_URI,
+	},
+} satisfies Config)
---
+++ migrate.ts
@@ -0,0 +1,9 @@
+import { drizzle } from "drizzle-orm/postgres-js"
+import { migrate } from "drizzle-orm/postgres-js/migrator"
+import postgres from "postgres"
+
+import env from "@/env"
+
+const migrationClient = postgres(env.DATABASE_URI, { max: 1 })
+await migrate(drizzle(migrationClient), { migrationsFolder: "server/db/migrations" })
+console.log("migration completed!")
