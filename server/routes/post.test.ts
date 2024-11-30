c641ca77bb633c2f568904953836bb150d2edfb0
felipeazs <felipeazs@gmail.com>
2024-11-30 19:54:01 -0300

    afterall delete test

--- server/routes/post.test.ts
+++ server/routes/post.test.ts
@@ -1,7 +1,10 @@
-import { describe, expect, it } from "vitest"
 import { testClient } from "hono/testing"
+import { afterAll, describe, expect, it } from "vitest"
 
 import env from "@/env"
+import { eq } from "drizzle-orm"
+import db from "../db"
+import { post as postTable } from "../db/schema"
 import createApp from "../lib/create-app"
 import router from "./post.index"
 
@@ -28,5 +31,9 @@ describe("Posts", () => {
 		const json = await res.json()
 
 		expect(json.status).toBe(true)
+	})
+
+	afterAll(async () => {
+		await db.delete(postTable).where(eq(postTable.post, "test 1"))
 	})
 })
